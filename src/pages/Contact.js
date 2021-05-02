27a75cb19f61ceab461c07f33cfe5cc3fbf3a833
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-05-02 16:03:56 +0200

    Contact form on submit change content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -19,19 +19,21 @@ export default function Contact() {
 
   return (
     <div className='Contact'>
-      <div className='tag'>&lt;div class="<span className='header'>contact-me</span>"&gt;</div>
-      {!send ?  <p className='contact-text'>Feel free to reach out to me using this form.</p> : ''}
-        {!send ?
-          <form className="contact-form" onSubmit={sendEmail}>
-            <input type="text" name="name" placeholder='Name' className='name-input' autoComplete='false'/>
-            <input type="email" name="email" placeholder='E-mail' className='email-input'  autoComplete='false'/>
-            <textarea name="message" placeholder='Message' className='message-input'  autoComplete='false'/>
-            <input type="submit" value="Send message" className='btn'/>
-          </form>
-          :
+      {!send ?
+        <>
+        <div className='tag'>&lt;div class="<span className='header'>contact-me</span>"&gt;</div>
+        <p className='contact-text'>Feel free to reach out to me using this form.</p> 
+            <form className="contact-form" onSubmit={sendEmail}>
+              <input type="text" name="name" placeholder='Name' className='name-input' autoComplete='false'/>
+              <input type="email" name="email" placeholder='E-mail' className='email-input'  autoComplete='false'/>
+              <textarea name="message" placeholder='Message' className='message-input'  autoComplete='false'/>
+              <input type="submit" value="Send message" className='btn'/>
+            </form>
+            <div className='tag'>&lt;/div&gt;</div>
+          </>
+        :
           <div className='success-message'>Thank you for the message. I will get back to you as soon as possible!</div>
         }
-       <div className='tag'>&lt;/div&gt;</div>
     </div>
     
   );
